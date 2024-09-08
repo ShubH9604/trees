ef21e24cee7ed6e400828f835d76293c0ce7f801
ShubH9604 <153529720+ShubH9604@users.noreply.github.com>
2024-09-08 22:28:02 +0530

    Add files via upload

---
+++ BinaryTreesPostorder.java
@@ -0,0 +1,64 @@
+public class BinaryTreesPostorder{
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+        static class BinaryTree {
+            static int idx = -1;
+            public static Node buildtree(int nodes[]){
+                idx++;
+                if(nodes[idx] == -1){
+                    return null;
+                }
+
+                Node newNode = new Node(nodes[idx]);
+                newNode.left = buildtree(nodes);
+                newNode.right = buildtree(nodes);
+
+                return newNode;
+            }
+        }
+
+        //Preorder
+        public static void preorder(Node root){
+            if(root == null){
+                return;
+            }
+            System.out.print(root.data + " ");
+            preorder(root.left);
+            preorder(root.right);
+        }
+
+        //Inorder
+        public static void inorder(Node root){
+            if(root == null){
+                return;
+            }
+            inorder(root.left);
+            System.out.print(root.data + " ");
+            inorder(root.right);
+        }
+
+        //Postorder
+        public static void postorder(Node root){
+            if(root == null){
+                return;
+            }
+            postorder(root.left);
+            postorder(root.right);
+            System.out.print(root.data + " ");
+        }
+    public static void main(String[] args) {
+    int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+    Node root = BinaryTree.buildtree(nodes);
+    postorder(root);
+    }
+}
